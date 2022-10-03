9a31062237976842f4faada42a18118b95098e9f
Korovkin Dmitry <dima6231054@gmail.com>
2022-10-03 22:47:48 +0400

    delete)

--- build/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const MatchReader_1 = require("./MatchReader");
-const Summary_1 = require("./Summary");
-const matches = MatchReader_1.MatchReader.fromCSV("football.csv");
-matches.load();
-const a = Summary_1.Summary.winsWithHtmlReport("Man United");
-a.buildAndPrintReport(matches.matches);
