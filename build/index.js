bcba507fc464087912d7306ee956a2ed26dd2684
Korovkin Dmitry <dima6231054@gmail.com>
2022-10-03 19:48:06 +0400

    Implementation via interface

--- build/index.js
+++ build/index.js
@@ -1,11 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const CSVFileReader_1 = require("./CSVFileReader");
+const MatchReader_1 = require("./MatchReader");
 const MatchResults_1 = require("./MatchResults");
-const matches = new CSVFileReader_1.CSVFileReader("football.csv");
-matches.read();
+// Create an obj that implements the DataReader interface
+const csvFileReader = new CSVFileReader_1.CSVFileReader("football.csv");
+// Create an obj of MatchReader and pass DataReader implementation
+const matches = new MatchReader_1.MatchReader(csvFileReader);
+matches.load();
 let manUnitedWins = 0;
-for (let match of matches.data) {
+for (let match of matches.matches) {
     if ((match[1] === "Man United" && match[5] === MatchResults_1.MatchResult.HomeWin) ||
         (match[2] === "Man United" && match[5] === MatchResults_1.MatchResult.AwayWin)) {
         manUnitedWins++;
@@ -14,4 +18,3 @@ for (let match of matches.data) {
 console.log(`
   Man United won ${manUnitedWins} times.
 `);
-console.log(matches.data[0]);
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import { CSVFileReader } from "./CSVFileReader";
+import { MatchReader } from "./MatchReader";
 import { MatchResult } from "./MatchResults";
-const matches = new CSVFileReader("football.csv");
-matches.read();
+
+// Create an obj that implements the DataReader interface
+const csvFileReader = new CSVFileReader("football.csv");
+
+// Create an obj of MatchReader and pass DataReader implementation
+const matches = new MatchReader(csvFileReader);
+matches.load();
 
 let manUnitedWins = 0;
 
-for (let match of matches.data) {
+for (let match of matches.matches) {
   if (
     (match[1] === "Man United" && match[5] === MatchResult.HomeWin) ||
     (match[2] === "Man United" && match[5] === MatchResult.AwayWin)
@@ -16,5 +22,3 @@ for (let match of matches.data) {
 console.log(`
   Man United won ${manUnitedWins} times.
 `);
-
-console.log(matches.data[0]);
